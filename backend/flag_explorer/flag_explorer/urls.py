1d421f3fe61ea1857ba80aa9caa1a78a50ca57f7
Que5 <phumlanibrightdlamini@gmail.com>
2025-06-01 12:01:56 +0200

    Updated the documentation

--- backend/flag_explorer/flag_explorer/urls.py
+++ backend/flag_explorer/flag_explorer/urls.py
@@ -25,9 +25,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Flag Explorer",
+        title="Country API",
         default_version='v1',
-        description="This is the API documentation for Flag Explorer",
+        description="This is the API documentation for Country API",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="phumlanibrightdlamini@gmail.com"),
         license=openapi.License(name="PB License"),
