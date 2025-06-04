c27a76b7c24f6a7e3db572a6cc5c151b6f79018a
Phumlani Bright Dlamini <123303760+Que5@users.noreply.github.com>
2025-06-04 12:38:25 +0200

    Update store.ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,18 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import countryReducer from './reducers'; // Import the TypeScript reducer
+import countryReducer from './reducers';
 
 const store = configureStore({
   reducer: {
     countries: countryReducer,
-    // Add other reducers here if you have them, e.g.:
-    // otherSlice: otherReducer,
   },
 });
 
-// Infer the `RootState` type from the store's `getState` method
 export type RootState = ReturnType<typeof store.getState>;
 
-// Infer the `AppDispatch` type from the store itself
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
